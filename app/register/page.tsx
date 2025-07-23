5657286456330da359e0fe4419feac376c5cc0ed
kl3inIT <dathip04@gmail.com>
2025-07-23 19:58:25 +0700

    fix layout

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,10 +1,8 @@
 import { RegisterForm } from '@/components/auth/register-form'
-import { Navbar } from '@/components/layout/navbar'
 
 export default function RegisterPage() {
   return (
     <div className='min-h-screen bg-background'>
-      <Navbar />
       <div className='container mx-auto px-4 py-16'>
         <RegisterForm />
       </div>
